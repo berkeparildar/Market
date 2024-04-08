7f2a74528c204e734195f497b54ad6ce778e3b3d
Berke Parıldar <94047726+berkeparildar@users.noreply.github.com>
2024-04-08 22:46:09 +0300

    grouped required scenes

---
+++ Getir-Final-Case/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Getir-Final-Case
+//
+//  Created by Berke Parıldar on 8.04.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
